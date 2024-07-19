5c556353580ba5e8d5a846408e29c358eba230ed
Tito <oficinac@gmail.com>
2024-07-19 18:29:12 -0300

    Add files via upload

---
+++ exemplo1.py
@@ -0,0 +1,26 @@
+import pgzrun
+
+alien = Actor('alien')
+alien.topright = 0, 10
+
+WIDTH = 500
+HEIGHT = alien.height + 20
+
+def draw():
+    screen.clear()
+    alien.draw()
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        sounds.eep.play()
+        alien.image = 'alien_hurt'
+    else:
+        alien.image = 'alien'
+
+pgzrun.go()
